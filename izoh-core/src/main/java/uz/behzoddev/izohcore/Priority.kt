b73b919f63fcc08b9195761221524dc37f941ca9
Behzod Halil <behzoddev@gmail.com>
2023-03-24 11:56:39 +0900

    Add Priority

---
+++ izoh-core/src/main/java/uz/behzoddev/izohcore/Priority.kt
@@ -0,0 +1,11 @@
+package uz.behzoddev.izohcore
+
+public enum class Priority(
+    public val level: Int
+) {
+    VERBOSE(level = 1),
+    DEBUG(level = 2),
+    INFO(level = 3),
+    WARNING(level = 4),
+    FAILURE(level = 5)
+}
