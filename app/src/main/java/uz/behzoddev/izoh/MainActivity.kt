33a2dd59eb3c6405350089ce7e5ee8d78e663284
Behzod Halil <behzoddev@gmail.com>
2023-03-24 11:59:05 +0900

    Add testing samples

--- app/src/main/java/uz/behzoddev/izoh/MainActivity.kt
+++ app/src/main/java/uz/behzoddev/izoh/MainActivity.kt
@@ -2,10 +2,22 @@ package uz.behzoddev.izoh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.behzoddev.izohcore.IzohLog
+import uz.behzoddev.izohcore.Platform
+import uz.behzoddev.izohcore.extension.debug
+import uz.behzoddev.izohcore.extension.izoh
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        IzohLog.install {
+            logger(platform = Platform.ANDROID)
+            enabled()
+        }
+
+        izoh { "" }
+        debug { "Testing" }
     }
 }
