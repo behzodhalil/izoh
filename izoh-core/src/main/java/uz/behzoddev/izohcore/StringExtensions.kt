ff355f54d33d093bd9e2bff99aa18cc979746422
Behzod Halil <behzoddev@gmail.com>
2023-03-24 11:57:16 +0900

    Add string extensions

---
+++ izoh-core/src/main/java/uz/behzoddev/izohcore/StringExtensions.kt
@@ -0,0 +1,22 @@
+package uz.behzoddev.izohcore
+
+import java.io.PrintWriter
+import java.io.StringWriter
+
+private const val MAX_BUFFER_SIZE = 256
+
+public fun Throwable.stringify(): String {
+    val sw = StringWriter(MAX_BUFFER_SIZE)
+    val pw = PrintWriter(sw, false)
+    printStackTrace(pw)
+    pw.flush()
+    return sw.toString()
+}
+
+public fun Priority.stringify(): String = when (this) {
+    Priority.VERBOSE -> "V"
+    Priority.DEBUG -> "D"
+    Priority.INFO -> "I"
+    Priority.WARNING -> "W"
+    Priority.FAILURE -> "E"
+}
