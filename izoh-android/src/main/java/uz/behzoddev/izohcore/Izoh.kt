bd420797c2e067998068895c78699d96712749cc
Behzod Halil <behzoddev@gmail.com>
2023-03-27 17:48:41 +0900

    Add Izoh

---
+++ izoh-android/src/main/java/uz/behzoddev/izohcore/Izoh.kt
@@ -0,0 +1,40 @@
+package uz.behzoddev.izohcore
+
+import uz.behzoddev.Configuration
+import uz.behzoddev.FailureIzohher
+
+object Izoh {
+
+    private val configuration: Configuration = Configuration()
+
+    @Volatile
+    @PublishedApi
+    internal var currentIzohher: Izohher = FailureIzohher()
+        private set
+
+    @JvmStatic
+    public fun enabled() {
+        configuration.isEnabled = true
+    }
+
+    @JvmStatic
+    public fun logger() {
+
+    }
+
+    @JvmStatic
+    public fun verbose(tag: String, message: () -> String) {
+        currentIzohher.log(LoggingLevel.VERBOSE, tag = tag, message = message())
+    }
+
+    @JvmStatic
+    public fun debug(tag: String, message: () -> String) {
+        currentIzohher.log(LoggingLevel.DEBUG, tag = tag, message = message())
+    }
+
+    @JvmStatic
+    public fun warning(tag: String, message: () -> String) {
+        currentIzohher.log(LoggingLevel.WARNING, tag = tag, message = message())
+    }
+
+}
