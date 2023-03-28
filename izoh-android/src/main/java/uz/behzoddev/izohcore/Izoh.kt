dff662f05158857079fdf97909958a5b5981223c
Behzod Halil <behzoddev@gmail.com>
2023-03-28 16:01:47 +0900

    Update izoh class

--- izoh-android/src/main/java/uz/behzoddev/izohcore/Izoh.kt
+++ izoh-android/src/main/java/uz/behzoddev/izohcore/Izoh.kt
@@ -1,8 +1,5 @@
 package uz.behzoddev.izohcore
 
-import uz.behzoddev.Configuration
-import uz.behzoddev.FailureIzohher
-
 object Izoh {
 
   private val configuration: Configuration = Configuration()
@@ -18,21 +15,88 @@ object Izoh {
   }
 
   @JvmStatic
-  public fun logger() {
+  public fun logger(platform: Platform, tagLength: Int) {
+    when (platform) {
+      Platform.Kotlin -> {
+        KotlinIzohher()
+      }
+      Platform.Android -> {
+        AndroidIzohher(tagLength)
+      }
+    }
   }
 
   @JvmStatic
-  public fun verbose(tag: String, message: () -> String) {
-    currentIzohher.log(LoggingLevel.VERBOSE, tag = tag, message = message())
+  public fun install(initializer: Izoh.() -> Unit) {
+    synchronized(this) {
+      initializer.invoke(this)
+      if (configuration.isEnabled) {
+        failure("Failed the installation of izoh logger library") {
+          "The logger $currentIzohher is already installed but you've tried to install a new logger:\""
+        }
+      }
+      if (configuration.izohher != null) {
+        currentIzohher = requireNotNull(configuration.izohher)
+      }
+    }
   }
 
   @JvmStatic
-  public fun debug(tag: String, message: () -> String) {
-    currentIzohher.log(LoggingLevel.DEBUG, tag = tag, message = message())
+  public fun uninstall() {
+    synchronized(this) {
+      currentIzohher = EmptyIzohher()
+      configuration.isEnabled = false
+    }
   }
 
   @JvmStatic
-  public fun warning(tag: String, message: () -> String) {
-    currentIzohher.log(LoggingLevel.WARNING, tag = tag, message = message())
+  public inline fun verbose(tag: String, message: () -> String) {
+    currentIzohher.log(Level.VERBOSE, tag = tag, message = message())
+  }
+
+  @JvmStatic
+  public inline fun debug(tag: String, message: () -> String) {
+    currentIzohher.log(Level.DEBUG, tag = tag, message = message())
+  }
+
+  @JvmStatic
+  public inline fun warning(tag: String, message: () -> String) {
+    currentIzohher.log(Level.WARNING, tag = tag, message = message())
+  }
+
+  @JvmStatic
+  public inline fun info(tag: String, message: () -> String) {
+    currentIzohher.log(Level.INFO, tag = tag, message = message())
+  }
+
+  @JvmStatic
+  public inline fun failure(tag: String, throwable: Throwable?, message: () -> String) {
+    currentIzohher.log(Level.FAILURE, tag = tag, message = message(), throwable = throwable)
+  }
+
+  @JvmStatic
+  public inline fun failure(tag: String, message: () -> String) {
+    currentIzohher.log(Level.FAILURE, tag = tag, message = message())
+  }
+
+  @JvmStatic
+  public inline fun log(
+    level: Level,
+    tag: String,
+    message: () -> String,
+    throwable: Throwable? = null
+  ) {
+    if (throwable != null) {
+      failure(tag, message)
+    } else {
+      when (level) {
+        Level.VERBOSE -> verbose(tag, message)
+        Level.DEBUG -> debug(tag, message)
+        Level.INFO -> info(tag, message)
+        Level.WARNING -> warning(tag, message)
+        Level.FAILURE -> failure(tag, message)
+      }
+    }
   }
 }
+
