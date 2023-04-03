03c3c45fa330da24ea6b062f1641199afbd27ed4
Behzod Halil <behzoddev@gmail.com>
2023-04-03 10:57:46 +0900

    Update version catalog

--- izoh-common/build/generated/source/buildConfig/debug/uz/behzoddev/izohcommon/BuildConfig.java
+++ izoh-common/build/generated/source/buildConfig/debug/uz/behzoddev/izohcommon/BuildConfig.java
@@ -5,6 +5,6 @@ package uz.behzoddev.izohcommon;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "uz.behzoddev.izoh_common";
+  public static final String LIBRARY_PACKAGE_NAME = "uz.behzoddev.izohcommon";
   public static final String BUILD_TYPE = "debug";
 }
