9ca95a67bff1384d5ba5443d330c63c92fdd08fe
Behzod Halil <behzoddev@gmail.com>
2023-04-06 17:42:18 +0900

    Add izoh using examples

--- app/src/main/java/uz/behzoddev/izoh/MainActivity.kt
+++ app/src/main/java/uz/behzoddev/izoh/MainActivity.kt
@@ -2,13 +2,19 @@ package uz.behzoddev.izoh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import uz.behzoddev.izohcore.izoh
+import uz.behzoddev.izohandroid.debug
+import uz.behzoddev.izohandroid.failure
+import uz.behzoddev.izohandroid.success
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    izoh { "Testing the izoh" }
+    success { "There is a success log function" }
+
+    failure { "There is a failure log function" }
+
+    debug { "There is a debug log function" }
   }
 }
